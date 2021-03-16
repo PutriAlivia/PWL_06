1ba2521734a2452eef72e520173426f25194da35
Putri Alivia <putrialivia666@gmail.com>
2021-03-16 15:25:33 +0700

    Penambahan kolom  dengan nama username

--- database/migrations/2021_03_16_080644_tambah_kolom_di_tabel_user.php
+++ database/migrations/2021_03_16_080644_tambah_kolom_di_tabel_user.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class TambahKolomDiTabelUser extends Migration
 {
     /**
@@ -13,7 +14,7 @@ class TambahKolomDiTabelUser extends Migration
      */
     public function up()
     {
-       Schema::table('user', function(Blueprint $table){
+       Schema::table('users', function(Blueprint $table){
            $table->string('username', 20)->after('id')->nullable()->unique();
        });
     }
@@ -25,7 +26,7 @@ class TambahKolomDiTabelUser extends Migration
      */
     public function down()
     {
-        Schema::table('user', function(Blueprint $table){
+        Schema::table('users', function(Blueprint $table){
             $table->dropColumn('username');
         });
     }
